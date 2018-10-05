c22451fae0c151eddb6bac0374fa5f93e5704244
gwenchee <gwendolynchee95@gmail.com>
2018-10-05 15:31:45 -0500

    unit test fix

--- tests/solver_unit_tests.py
+++ tests/solver_unit_tests.py
@@ -3,15 +3,10 @@ import sys
 import os
 import pytest
 dirname = os.path.dirname(os.path.abspath(__file__))
-print(dirname)
 dirname = dirname.split('/tests')[0]
 dirname = dirname + '/d3ploy'
 sys.path.append(dirname)
-from no_inst import NOInst
-from cyclus.agents import Institution, Agent
-from cyclus import lib
-
-# deploy_solver_fcn = NOInst.deploy_solver
+import solver
 
 
 def test_deploy_solver():
@@ -20,9 +15,7 @@ def test_deploy_solver():
         commod = {}
         for i in range(4):
             commod.update({str(i): random.uniform(0.1, 9.9)})
-        obj = NOInst
-        obj.commodity_dict = {'commod': commod}
-        deploy_dict = obj.deploy_solver(obj, 'commod', diff)
+        deploy_dict = solver.deploy_solver({'commod':commod}, 'commod', diff)
         # actually deploy and see if it's good
         final_diff = diff
         for key, val in deploy_dict.items():
