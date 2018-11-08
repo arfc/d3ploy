8707dec25a7141aef92805d12ce10a3beca2428a
jbae11 <jbae11@illinois.edu>
2018-11-08 15:03:01 -0600

    fix degree

--- d3ploy/DO_solvers.py
+++ d3ploy/DO_solvers.py
@@ -91,7 +91,7 @@ def fft(ts, back_steps=1e6, degree=1):
     n = timeseries.size
     n_harm = 100                    # number of harmonics in model
     t = np.arange(0, n)
-    p = np.polyfit(t, timeseries, 2)         # find linear trend in x
+    p = np.polyfit(t, timeseries, degree)         # find linear trend in x
     x_notrend = timeseries - p[0] * t        # detrended x
     x_freqdom = np.fft.fft(x_notrend)  # detrended x in frequency domain
     f = np.fft.fftfreq(n)              # frequencies
