83987a96b78c53ed18b27065776fb21691ef8994
gwenchee <gwendolynchee95@gmail.com>
2018-11-24 14:52:55 -0500

    at 6 reactor 2

--- tests/integration_tests/tech_pref_test.py
+++ tests/integration_tests/tech_pref_test.py
@@ -214,4 +214,12 @@ def test_tech_pref_cross():
     # check if ran successfully and deployed facilities at time step 1 
     cur = functions.get_cursor(output_file)
     agent_entry = cur.execute("select entertime, prototype from agententry").fetchall()
-    
+
+    # check that it deploys a reactor 2 at time step 6 
+    passes = 0 
+    for x in range(0,len(agent_entry)): 
+        if agent_entry[x][0] == 6: 
+            if agent_entry[x][1] == 'reactor2':
+                passes = 1 
+                break 
+    assert(passes == 1)
