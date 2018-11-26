9e409f3091f3a3c01786ea78560a56ba5a103ec5
jbae11 <jbae11@illinois.edu>
2018-11-26 15:42:34 -0600

    function name

--- tests/integration_tests/continuous_integration_test.py
+++ tests/integration_tests/continuous_integration_test.py
@@ -23,7 +23,7 @@ ENV['PYTHONPATH'] = ".:" + ENV.get('PYTHONPATH', '')
 # Checking if cyclus simulations and cyclus output files are created and populated for each calc method
 
 calc_methods = ["ma", "arma", "arch", "poly",
-                "exp_smoothing", "holt_winters", 'fft']
+                "exp_smoothing", "holt_winters", "fft"]
 
 test_cont_input = {}
 
@@ -155,5 +155,5 @@ def test_cont_integ_holt_winters():
     assert(total['holt_winters'] == 4)
 
 
-def test_cont_fft():
+def test_cont_integ_fft():
     assert(total['fft'] == 4)
