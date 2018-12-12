5cb07164eb2540192f0d6b7e8e054dbf14d6355d
gwenchee <gwendolynchee95@gmail.com>
2018-12-12 17:16:20 -0600

    closed fuel cycle meets power demand

--- tests/performance_tests/algorithm_performance_tests_closedfuelcycle.py
+++ tests/performance_tests/algorithm_performance_tests_closedfuelcycle.py
@@ -39,3 +39,259 @@ ENV['PYTHONPATH'] = ".:" + ENV.get('PYTHONPATH', '')
 ##### List of types of calc methods that are to be tested #####
 calc_methods = ["ma", "arma", "arch", "poly",
                 "exp_smoothing", "holt_winters", "fft"]
+
+######################################SCENARIO 7##########################################
+scenario_7_input = {}
+demand_eq = "1000*t"
+
+for calc_method in calc_methods:
+    scenario_7_input[calc_method] = {
+ "simulation": {
+  "archetypes": {
+   "spec": [
+    {"lib": "agents", "name": "NullRegion"},
+    {"lib": "cycamore", "name": "Source"},
+    {"lib": "cycamore", "name": "Reactor"},
+    {"lib": "cycamore", "name": "Mixer"},
+    {"lib": "cycamore", "name": "Separations"},
+    {"lib": "cycamore", "name": "Storage"},
+    {"lib": "cycamore", "name": "Sink"},
+    {"lib": "d3ploy.timeseries_inst", "name": "TimeSeriesInst"},
+    {
+     "lib": "d3ploy.supply_driven_deployment_inst",
+     "name": "SupplyDrivenDeploymentInst"
+    }
+   ]
+  },
+  "control": {"duration": "20", "startmonth": "1", "startyear": "2000"},
+  "facility": [
+   {
+    "config": {
+     "Source": {
+      "outcommod": "sourceoutput",
+      "outrecipe": "source_recipe",
+      "throughput": "3000"
+     }
+    },
+    "name": "source"
+   },
+   {
+    "config": {
+     "Separations": {
+      "feed_commod_prefs": {"val": "1.0"},
+      "feed_commods": {"val": "reactor1output"},
+      "feed_recipe": "source_recipe",
+      "feedbuf_size": "1e6",
+      "leftover_commod": "reprocess_waste1",
+      "leftoverbuf_size": "1e6",
+      "streams": {
+       "item": [
+        {
+         "commod": "separations1Pu",
+         "info": {"buf_size": "1e6", "efficiencies": {"item": {"comp": "Pu", "eff": "1"}}}
+        },
+        {
+         "commod": "separations1U",
+         "info": {"buf_size": "1e6", "efficiencies": {"item": {"comp": "U", "eff": "1"}}}
+        }
+       ]
+      },
+      "throughput": "1e6"
+     }
+    },
+    "name": "separations1"
+   },
+   {
+    "config": {
+     "Mixer": {
+      "in_streams": {
+       "stream": [
+        {
+         "commodities": {
+          "item": [
+           {"commodity": "separations1Pu", "pref": "1.0"},
+           {"commodity": "separations2Pu", "pref": "1.0"}
+          ]
+         },
+         "info": {"buf_size": "1e6", "mixing_ratio": "0.5"}
+        },
+        {
+         "commodities": {
+          "item": [
+           {"commodity": "separations1U", "pref": "1.0"},
+           {"commodity": "separations2Pu", "pref": "1.0"}
+          ]
+         },
+         "info": {"buf_size": "1e6", "mixing_ratio": "0.5"}
+        }
+       ]
+      },
+      "out_buf_size": "1e6",
+      "out_commod": "mixeroutput",
+      "throughput": "1e6"
+     }
+    },
+    "name": "mixer"
+   },
+   {
+    "config": {
+     "Reactor": {
+      "assem_size": "1000",
+      "cycle_time": "1",
+      "fuel_incommods": {"val": "sourceoutput"},
+      "fuel_inrecipes": {"val": "source_recipe"},
+      "fuel_outcommods": {"val": "reactor1output"},
+      "fuel_outrecipes": {"val": "reactor_recipe"},
+      "n_assem_batch": "1",
+      "n_assem_core": "3",
+      "power_cap": "1000",
+      "refuel_time": "0"
+     }
+    },
+    "name": "reactor1"
+   },
+   {
+    "config": {
+     "Reactor": {
+      "assem_size": "1000",
+      "cycle_time": "1",
+      "fuel_incommods": {"val": "mixeroutput"},
+      "fuel_inrecipes": {"val": "source_recipe"},
+      "fuel_outcommods": {"val": "reactor2output"},
+      "fuel_outrecipes": {"val": "reactor_recipe"},
+      "n_assem_batch": "1",
+      "n_assem_core": "3",
+      "power_cap": "1000",
+      "refuel_time": "0"
+     }
+    },
+    "name": "reactor2"
+   },
+   {
+    "config": {
+     "Separations": {
+      "feed_commod_prefs": {"val": "1.0"},
+      "feed_commods": {"val": "reactor2output"},
+      "feed_recipe": "source_recipe",
+      "feedbuf_size": "1e6",
+      "leftover_commod": "reprocess_waste2",
+      "leftoverbuf_size": "1e6",
+      "streams": {
+       "item": [
+        {
+         "commod": "separations2Pu",
+         "info": {"buf_size": "1e6", "efficiencies": {"item": {"comp": "Pu", "eff": "1"}}}
+        },
+        {
+         "commod": "separations2U",
+         "info": {"buf_size": "1e6", "efficiencies": {"item": {"comp": "U", "eff": "1"}}}
+        }
+       ]
+      },
+      "throughput": "1e6"
+     }
+    },
+    "name": "separations2"
+   },
+   {
+    "config": {
+     "Sink": {
+      "in_commods": {"val": ["reprocess_waste1", "reprocess_waste2"]},
+      "max_inv_size": "1e6"
+     }
+    },
+    "name": "sink"
+   }
+  ],
+  "recipe": [
+   {
+    "basis": "mass",
+    "name": "source_recipe",
+    "nuclide": [{"comp": "0.5", "id": "U235"}, {"comp": "0.5", "id": "Pu238"}]
+   },
+   {
+    "basis": "mass",
+    "name": "reactor_recipe",
+    "nuclide": [{"comp": "0.5", "id": "U235"}, {"comp": "0.5", "id": "Pu238"}]
+   }
+  ],
+  "region": {
+   "config": {"NullRegion": "\n      "},
+   "institution": [
+    {
+     "config": {
+      "TimeSeriesInst": {
+       "calc_method": calc_method,
+       "commodities": {
+        "val": [
+         "POWER_reactor2_3000_2*t_mixeroutput_30000",
+         "POWER_reactor1_3000_t",
+         "sourceoutput_source_3000"
+        ]
+       },
+       "demand_eq": "1000*t",
+       "demand_std_dev": "0.0",
+       "record": "1",
+       "steps": "1"
+      }
+     },
+     "name": "demand_inst"
+    },
+    {
+     "config": {
+      "SupplyDrivenDeploymentInst": {
+       "calc_method": calc_method,
+       "capacity_std_dev": "1.0",
+       "commodities": {
+        "val": [
+         "reactor1output_separations1_1e6",
+         "separations1Pu_mixer_1e6",
+         "reactor2output_separations2_1e6"
+        ]
+       },
+       "record": "1",
+       "steps": "1"
+      }
+     },
+     "name": "supply_inst"
+    }
+   ],
+   "name": "SingleRegion"
+  }
+ }
+}
+
+# initialize metric dict
+metric_dict = {}
+for calc_method in calc_methods:
+    name = "scenario_7_input_" + calc_method
+    input_file = name + ".json"
+    output_file = name + ".sqlite"
+    with open(input_file, 'w') as f:
+        json.dump(scenario_7_input[calc_method], f)
+    s = subprocess.check_output(['cyclus', '-o', output_file, input_file],
+                                universal_newlines=True, env=ENV)
+    # Initialize dicts  
+    all_dict_power = {}
+    all_dict_fuel = {} 
+    all_dict_spentfuel = {} 
+    all_dict_power = tester.supply_demand_dict_driving(
+        output_file, demand_eq, 'power')
+    all_dict_fuel = tester.supply_demand_dict_nondriving(
+        output_file, 'fuel',True)
+    all_dict_spentfuel = tester.supply_demand_dict_nondriving(
+        output_file, 'spentfuel',False)
+    
+    # plots demand, supply, calculated demand, calculated supply for the scenario for each calc method
+    plotter.plot_demand_supply(all_dict_power, 'power', name, True)
+    name2 = "scenario_7_input_"+ calc_method +"_fuel"
+    plotter.plot_demand_supply(all_dict_fuel, 'fuel', name2, True)
+    name3 = "scenario_7_input_"+ calc_method +"_spentfuel"
+    plotter.plot_demand_supply(all_dict_spentfuel, 'spentfuel', name3, False)
+    
+    metric_dict = tester.metrics(all_dict_power,metric_dict,calc_method,'power',True)
+    metric_dict = tester.metrics(all_dict_fuel,metric_dict,calc_method,'fuel',True)
+    metric_dict = tester.metrics(all_dict_spentfuel,metric_dict,calc_method,'spentfuel',False)
+        
+    df = pd.DataFrame(metric_dict)
+    df.to_csv('scenario_5_output.csv')
