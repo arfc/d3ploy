090a404d38e8cf6431213fba9caa66ea3b9a4b4d
Jin Whan Bae <jbae11@illinois.edu>
2018-12-13 10:43:17 -0600

    Update tests/performance_tests/algorithm_performance_tests_closedfuelcycle.py
    Co-Authored-By: gwenchee <gwendolynchee95@gmail.com>

--- tests/performance_tests/algorithm_performance_tests_closedfuelcycle.py
+++ tests/performance_tests/algorithm_performance_tests_closedfuelcycle.py
@@ -283,7 +283,7 @@ for calc_method in calc_methods:
     # plots demand, supply, calculated demand, calculated supply for the scenario for each calc method
     plotter.plot_demand_supply(all_dict_power, 'power', name, True)
     name2 = "scenario_7_input_"+ calc_method +"_fuel"
-    plotter.plot_demand_supply(all_dict_fuel, 'fuel', name2, True)
+    plotter.plot_demand_supply(all_dict['fuel'], 'fuel', name2, True)
     name3 = "scenario_7_input_"+ calc_method +"_spentfuel"
     plotter.plot_demand_supply(all_dict_spentfuel, 'spentfuel', name3, False)
     
