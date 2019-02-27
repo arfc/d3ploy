31111479e31ab22b59a0fff6b3dab7bfb46ccba5
robfairh <ref3@illinois.edu>
2019-02-27 10:25:44 -0600

    PEP8 Changes 1

--- d3ploy/plotter.py
+++ d3ploy/plotter.py
@@ -3,20 +3,22 @@ import numpy as np
 
 
 def plot_demand_supply(all_dict, commod, test, demand_driven):
-    """ Plots demand, supply, calculated demand and calculated supply on a curve 
+    """ Plots demand, supply, calculated demand and calculated
+    supply on a curve
+
     Parameters
     ----------
     all_dict: (4 dicts) dictionaries of supply, demand, calculated
     demand, and calculated supply
     commod: name of the commodity to plot
     test: name of the output image file
-    demand_driven: Boolean. If true, the commodity is demand driven, 
+    demand_driven: Boolean. If true, the commodity is demand driven,
     if false, the commodity is supply driven
     Returns
     -------
-    plot of all four dicts 
+    plot of all four dicts
     """
-    
+
     dict_demand = all_dict['dict_demand']
     dict_supply = all_dict['dict_supply']
     dict_calc_demand = all_dict['dict_calc_demand']
@@ -24,7 +26,8 @@ def plot_demand_supply(all_dict, commod, test, demand_driven):
 
     fig, ax = plt.subplots(figsize=(15, 7))
     if demand_driven:
-        ax.plot(*zip(*sorted(dict_demand.items())), '+', color='red', label='Demand')
+        ax.plot(*zip(*sorted(dict_demand.items())), '+', color='red',
+         label='Demand')
         ax.plot(*zip(*sorted(dict_calc_demand.items())),
                 'o', color='red', label='Calculated Demand')
         ax.set_title('%s Demand Supply plot' %test)
@@ -34,12 +37,13 @@ def plot_demand_supply(all_dict, commod, test, demand_driven):
         ax.plot(*zip(*sorted(dict_calc_demand.items())),
                 'o', color='red', label='Calculated Capacity')
         ax.set_title('%s Capacity Supply plot' %test)
-    ax.plot(*zip(*sorted(dict_supply.items())), 'x', color='c', label='Supply')
+    ax.plot(*zip(*sorted(dict_supply.items())), 'x', color='c',
+     label='Supply')
     ax.plot(*zip(*sorted(dict_calc_supply.items())),
             'o', alpha=0.5, color='c', label='Calculated Supply')
     ax.grid()
     ax.set_xlabel('Time (month timestep)', fontsize=14)
-    if commod=='power':
+    if commod == 'power':
         ax.set_ylabel('Power (MW)', fontsize=14)
     else:
         ax.set_ylabel('Mass (Kg)', fontsize=14)
@@ -57,29 +61,30 @@ def plot_demand_supply(all_dict, commod, test, demand_driven):
     plt.close()
 
 
-def plot_demand_supply_agent(all_dict, agent_dict, commod, test, demand_driven):
+def plot_demand_supply_agent(all_dict,agent_dict,commod,test,demand_driven):
     """ Plots agents deployed to handle the commodity (commod)
-        Plots demand, supply, calculated demand and calculated supply on a curve 
+        Plots demand, supply, calculated demand and calculated supply
+         on a curve
     Parameters
     ----------
     all_dict: (4 dicts) dictionaries of supply, demand, calculated
     demand, and calculated supply
     commod: name of the commodity to plot
     test: name of the output image file
-    demand_driven: Boolean. If true, the commodity is demand driven, 
+    demand_driven: Boolean. If true, the commodity is demand driven,
     if false, the commodity is supply driven
     Returns
     -------
-    plot of agents and all four dicts 
+    plot of agents and all four dicts
     """
     
     dict_demand = all_dict['dict_demand']
     dict_supply = all_dict['dict_supply']
     dict_calc_demand = all_dict['dict_calc_demand']
     dict_calc_supply = all_dict['dict_calc_supply']
-    f, (ax1, ax2) = plt.subplots(2,1, sharex='all',
-                                 gridspec_kw = {'height_ratios': [1,3]})
-    
+    f, (ax1, ax2) = plt.subplots(2,1, sharex='all', 
+                                 gridspec_kw={'height_ratios': [1,3]})
+
     top_indx = True
     for key, val in agent_dict.items():
         x, y = get_xy_from_dict(val)
@@ -96,23 +101,23 @@ def plot_demand_supply_agent(all_dict, agent_dict, commod, test, demand_driven):
     ax1.legend()
     ax1.set_xlabel('Time (month timestep)')
     ax1.set_ylabel('agents')
-    
+
     marksize = 6
 
     if demand_driven:
         ax2.plot(*zip(*sorted(dict_demand.items())),
-                       label='Demand')
+            label='Demand')
         ax2.plot(*zip(*sorted(dict_calc_demand.items())),
-                      label='Calculated Demand')
+            label='Calculated Demand')
     else:
         ax2.plot(*zip(*sorted(dict_demand.items())),
-                      label='Capacity')
+            label='Capacity')
         ax2.plot(*zip(*sorted(dict_calc_demand.items())),
-                      label='Calculated Capacity')
+            label='Calculated Capacity')
     ax2.plot(*zip(*sorted(dict_supply.items())),
-                  label='Supply')
+        label='Supply')
     ax2.plot(*zip(*sorted(dict_calc_supply.items())),
-                 alpha=0.5,  label='Calculated Supply')
+        alpha=0.5,  label='Calculated Supply')
     ax2.grid()
     if commod=='power':
         ax.set_ylabel('Power (MW)')
@@ -120,11 +125,12 @@ def plot_demand_supply_agent(all_dict, agent_dict, commod, test, demand_driven):
         ax.set_ylabel('Mass (Kg)')
     handles, labels = ax2.get_legend_handles_labels()
     ax2.legend(handles, labels, fontsize=11, loc='upper left',
-               fancybox=True)
-    
+        fancybox=True)
+
     ax1.set_title('Supply, Demand and prototypes for %s' %test)
     plt.savefig(test, dpi=300, bbox_inches='tight')
     plt.close()
+
 
 def get_xy_from_dict(dictionary):
     maxindx = max(dictionary.keys()) + 1
--- tests/performance_tests/algorithm_performance_tests_transitionscenario.py
+++ tests/performance_tests/algorithm_performance_tests_transitionscenario.py
@@ -1,10 +1,11 @@
 """
-This python file that compares calc methods for difference scenarios. 
-How to use: 
+This python file that compares calc methods for difference scenarios.
+How to use:
 python [file name]
-A scenario_#_output.txt file is output for each scenario. 
-In each text file, the chi2 goodness of fit and supply falling below demand results are shown 
-and the best calc method is determined for each type of results. 
+A scenario_#_output.txt file is output for each scenario.
+In each text file, the chi2 goodness of fit and supply falling below
+demand results are shown and the best calc method is determined for
+each type of results.
 """
 
 import json
@@ -24,26 +25,24 @@ import collections
 
 # Delete previously generated files
 direc = os.listdir('./')
-hit_list = glob.glob('*.json') + glob.glob('*.png') + glob.glob('*.csv') + glob.glob('*.txt')
+hit_list = glob.glob('*.json') + glob.glob('*.png')
+    + glob.glob('*.csv') + glob.glob('*.txt')
 for file in hit_list:
     os.remove(file)
 
 ENV = dict(os.environ)
 ENV['PYTHONPATH'] = ".:" + ENV.get('PYTHONPATH', '')
 
-
-######################################TRANSITION SCENARIO 1##########################################
-
+#############################TRANSITION SCENARIO 1############################
 # initialize metric dict
 demand_eq = '60000'
 calc_method = 'ma'
 name = "transitionscenario_1_input"
 output_file = name + ".sqlite"
-# Initialize dicts  
+# Initialize dicts
 metric_dict = {}
 all_dict = {}
 agent_entry_dict = {}
-
 
 # get agent deployment
 commod_dict = {'enrichmentout': ['enrichment'],
@@ -54,30 +53,32 @@ commod_dict = {'enrichmentout': ['enrichment'],
                'lwrout': ['lwrstorage'],
                'frout': ['frstorage'],
                'lwrreprocessingoutpu': ['pumixerlwr'],
-               'frreprocessingoutpu' : ['pumixerfr'],
+               'frreprocessingoutpu': ['pumixerfr'],
                'lwrreprocessingwaste': ['lwrsink'],
                'frreprocessingwaste': ['frsink']}
 for commod, facility in commod_dict.items():
     agent_entry_dict[commod] = tester.get_agent_dict(output_file, facility)
+
 ###########################
 
 # get supply deamnd dict
 # and plot
 all_dict['power'] = tester.supply_demand_dict_driving(
     output_file, demand_eq, 'power')
-plotter.plot_demand_supply_agent(all_dict['power'], agent_entry_dict['power'], 'power',
-                                 'transitionscenario_1_input_power', True)
+plotter.plot_demand_supply_agent(all_dict['power'], agent_entry_dict['power'],
+        'power', 'transitionscenario_1_input_power', True)
 
 front_commods = ['sourceout', 'enrichmentout']
 back_commods = ['lwrstorageout',
                 'frstorageout', 'lwrout', 'frout', 'lwrreprocessingoutpu',
-                'frreprocessingoutpu', 'lwrreprocessingwaste', 'frreprocessingwaste']
+                'frreprocessingoutpu', 'lwrreprocessingwaste',
+                'frreprocessingwaste']
 for commod in front_commods:
     all_dict[commod] = tester.supply_demand_dict_nondriving(output_file,
                         commod, True)
     name = 'transitionscenario_1_input_' + commod
-    plotter.plot_demand_supply_agent(all_dict[commod], agent_entry_dict[commod],
-                                     commod, name, True)
+    plotter.plot_demand_supply_agent(all_dict[commod],
+        agent_entry_dict[commod], commod, name, True)
     metric_dict = tester.metrics(
         all_dict[commod], metric_dict, calc_method, commod, True)
 
@@ -85,8 +86,8 @@ for commod in back_commods:
     all_dict[commod] = tester.supply_demand_dict_nondriving(output_file,
                         commod, False)
     name = 'transitionscenario_1_input_' + commod
-    plotter.plot_demand_supply_agent(all_dict[commod], agent_entry_dict[commod],
-                                     commod, name, False)
+    plotter.plot_demand_supply_agent(all_dict[commod],
+            agent_entry_dict[commod], commod, name, False)
     metric_dict = tester.metrics(
         all_dict[commod], metric_dict, calc_method, commod, False)
 
